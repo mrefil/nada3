cb08fe3982387c7063a6a2f06e0adf8d0e4009b3
Mustafa E <61510151+mrefil@users.noreply.github.com>
2022-02-26 21:24:47 -0500

    add config

---
+++ Client/A3/A3/Config.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3
+{
+    static public class Config
+    {
+        public const string wl_msg = "Enter a words, or type \"help\" for help\n";
+        public const string cm_hlp = "Help";
+        public const string cm_nose = "Noise";
+        public const string cm_mnl = "Manual";
+        public const string cm_auto = "Auto";
+        public const string cm_qt = "Quit";
+        public const string cm_cnt = "";
+        public const string cm_err_route = @"..\..\..\..\textFiles\err_args.txt";
+        public const string cm_hlp_route = @"..\..\..\..\textFiles\hlp_file.txt";
+        public const string cm_eut_route = @"..\..\..\..\textFiles\aut_file.txt";
+        public const string cm_tst_route = @"..\..\..\..\textFiles\tst_logg.txt";
+        public const bool cm_submit = true;
+        public const bool cm_frmtted_lg = false;
+        public const string cm_msfrmt_lg = "Fromatted is not true";
+        public const string cm_invld_nmbr = "8785arentNum";
+        public const string cm_lvl_hgh = "11";
+        public const string cm_lvl_lw = "-2";
+        public const string cm_lvl_invd = "OFF";
+        public const string cm_plc_hldr = "Place Holder";
+        public const int cm_mx_nmbr = 1321;
+        public const string cm_fl_err = "File cannot open\n";
+    }
+}
